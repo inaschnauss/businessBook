2c363d6a2db49c878565b98cab3b83b38ad31109
inaschnauss <inaschnauss@gmail.com>
2017-05-05 20:24:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var pug = require("pug");
+
+
+app.get("/", function(req, res){ //diese funktion nennt sich callback
+	var compileFunction = pug.compileFile('./template/index_1.pug');
+	var data = {name: "Ina"};
+	var html = compileFunction(data);
+	res.send(html); //der ganze backend scheiß
+});
+//als beispiel für res.send '<strong style="color: red">Hello World!</strong>'
+//erster parameter = router
+//zweiter parameter = callback
+
+app.get("/login", function(req, res){
+	res.send("goodbye World")
+});
+
+app.listen(3000, function(){
+	console.log("bla bla port 3000")
+});
