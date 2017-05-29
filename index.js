d80a5db1dd38d3e4cad79a2088401921737d713b
doublelines <doubleline@protonmail.com>
2017-05-29 04:05:50 +0200

    pages, colours,

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var pug = require("pug");
 
 
+
 app.get("/", function(req, res){ //diese funktion nennt sich callback
 	var compileFunction = pug.compileFile('./template/index_1.pug');
 	var data = {name: "Ina"};
@@ -12,6 +13,19 @@ app.get("/", function(req, res){ //diese funktion nennt sich callback
 //als beispiel für res.send '<strong style="color: red">Hello World!</strong>'
 //erster parameter = router
 //zweiter parameter = callback
+
+app.get("/main", function(req, res){
+	var html = pug.renderFile('./template/main.pug')
+	res.send(html)
+});
+app.get("/contactus", function(req, res){
+	var html = pug.renderFile('./template/main.pug')
+	res.send(html)
+});
+app.get("/faqs", function(req, res){
+	var html = pug.renderFile('./template/main.pug')
+	res.send(html)
+});
 
 app.get("/login", function(req, res){
 	res.send("goodbye World")
