a4886d2693dddd653c8cc4ca03cfc5aa8633efe7
inaschnauss <inaschnauss@gmail.com>
2017-06-11 22:43:09 +0200

    add the content of _style.pug into _head.pug and changed the name of the js file

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $("#sign-up-button").click(function(){
+      $("input").each(function(index, element){
+        if(element.value.length == 0){
+    			$(element).addClass("border-error")
+        }else{
+          $(element).removeClass("border-error")
+          }
+        })
+    })
+})
