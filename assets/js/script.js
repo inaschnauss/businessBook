d487c72af298f8bc5979aa4c34853fe9914560f3
Shofa Tazkia <s_tazkia@yahoo.com>
2017-07-10 21:44:28 +0200

    edit order only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  console.log( "ready!" );
 
-    $("#sign-up-button").click(function(){
-      $("input").each(function(index, element){
-        if(element.value.length == 0){
-    			$(element).addClass("border-error")
-        }else{
-          $(element).removeClass("border-error")
-          }
-        })
-    })
+  $("#sign-up-button").click(function(){
+    $("input").each(function(index, element){
+      if(element.value.length == 0){
+        $(element).addClass("border-error")
+      }else{
+        $(element).removeClass("border-error")
+        }
+      })
+  })
+
+  $('select').material_select();
+
+
 })
-
-//Dropdown option search
-$(document).ready(function() {
-    $('select').material_select();
-  });
