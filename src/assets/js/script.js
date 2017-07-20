54373d6113db3d4ee0be4922a47a8c9678b43d71
inaschnauss <inaschnauss@gmail.com>
2017-07-21 00:31:54 +0200

    fixing the jquery in footer

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,6 @@
+//sign up button
 $( document ).ready(function() {
-  console.log( "ready!" );
-
+  console.log( "sign-up-button-register" );
   $("#sign-up-button").click(function(){
     $("input").each(function(index, element){
       if(element.value.length == 0){
@@ -11,14 +11,24 @@ $( document ).ready(function() {
       })
   })
   $('select').material_select();
-
 })
 
+//send button
+$( document ).ready(function() {
+  console.log( "sendButton-contactUS" );
+  $("#sendButton").click(function(){
+    $("input").each(function(index, element){
+      if(element.value.length == 0){
+        $(element).addClass("border-error")
+      }else{
+        $(element).removeClass("border-error")
+        }
+      })
+  })
+  $('select').material_select();
+})
 
-
-// $(document).ready(function(){
-//   console.log("test");
-// });
+//smooth scroll
 function smoothScroll(){
   console.log("hello");
   $.scrollTo($('#menudiv'), 500);
