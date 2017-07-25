3b0c098da7cff09e20c2cc426b10f9ba7e8e9e9e
inaschnauss <inaschnauss@gmail.com>
2017-07-25 18:59:37 +0200

    put the events to the calendar

--- src/assets/js/calendar.js
+++ src/assets/js/calendar.js
@@ -6,5 +6,26 @@ $( document ).ready(function() {
   if($("#calendar")[0]){
     $("#calendar").fullCalendar();
   }
-
 })
+
+$('#calendar').fullCalendar({
+  eventSources: [{
+    events: [{
+      title  : 'Founder workshop',
+      start  : '2017-07-01'
+    },
+    {
+      title  : 'event2',
+      start  : '2017-07-05',
+      end    : '2017-07-07'
+    },
+    {
+      title  : 'Founder workshop',
+      start  : '2017-07-09T12:00:00',
+      end    : '2017-07-09T18:00:00',
+      allDay : false // will make the time show
+    }],
+    color: "black",
+    textColor: "yellow"
+  }]
+});
